4283c3e5fc3af30292291e59ee71f1c38b5f7e26
anirudh-100 <anirudhrai1212@gmail.com>
2025-03-17 12:35:33 +0530

    last commit

--- src/app/v1/aboutus/page.tsx
+++ src/app/v1/aboutus/page.tsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 import { useRouter } from "next/navigation"; // Import from next/navigation
-import Link from "next/link";
+// import Link from "next/link";
 import Image from "next/image";
 
 const AboutUs = () => {
